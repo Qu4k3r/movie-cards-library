04b37251d66b0d03d7870429fe73fb2fd77fa840
Qu4k3r <neves0007@gmail.com>
2021-04-15 01:35:27 -0300

    Updates

---
+++ precache-manifest.5289bfef3951e36603e52c1ff59b4658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eef06e9889643d525b0778a5be8a25f",
+    "url": "/movie-cards-library/index.html"
+  },
+  {
+    "revision": "1df47d93063278f7dbd7",
+    "url": "/movie-cards-library/static/css/main.da8d717f.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "1df47d93063278f7dbd7",
+    "url": "/movie-cards-library/static/js/main.000f292d.chunk.js"
+  },
+  {
+    "revision": "a95db5ba2439b13bdc08",
+    "url": "/movie-cards-library/static/js/runtime-main.a37d5318.js"
+  }
+]);
